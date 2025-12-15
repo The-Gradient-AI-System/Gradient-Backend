3126ba8b5ac76de371941336f45adf9f0be147fd
VladyslavFiniahin <finyagin07@gmail.com>
2025-12-15 15:53:05 +0200

    organize code and adding JWT token

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ conn = duckdb.connect("db/database.duckdb")
 def init_db():
     conn.execute("""
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER,
-        username TEXT NOT NULL,
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL
     )
